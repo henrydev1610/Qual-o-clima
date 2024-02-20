eb219eb63248c8be01448e8aa570aff440f60b04
Henry Benjamin <rangelantonio.dev@gmail.com>
2024-02-20 15:30:35 -0300

    chamada de api

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('ola mundo ')
+
+const apiKey = 'f7269ff1547078d53d6e1a3a2ae47f13',
+apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+
+
+
+async function checkWeather(city){
+
+
+
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data)
+    
+
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp ) + '°c'
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%"
+    document.querySelector('.wind').innerHTML = data.wind.speed + " km/h"
+
+
+
+
+
+}
+
+
+searchBtn.addEventListener('click',()=>{
+    
+    checkWeather(searchBox.value)
+  
+
+
+
+})
+
